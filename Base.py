d51fba466aa1c74b474132a4cd50abe6a456f70b
DataBase18 <aramireza18@miumg.edu.gt>
2025-04-11 20:45:56 -0600

    Se agrega fotter, paridad, bits, y mensajes informativos. Listo en su version alpha para probar

--- Base.py
+++ Base.py
@@ -9,30 +9,53 @@ import keyboard
 
 class SerialRobotAbner:
     
-    def getPorts():  
+    def getPorts(self):  
       portList = serial.tools.list_ports.comports() 
       ports = []
       for puerto in portList:
           ports.append(puerto.name)
       return ports
     
+    def setPortsToCombo (self):
+        portList  = self.getPorts()
+        self.ports.configure(values=portList)
 
-    def listenerPort (self, portName, baud, bits, bitStop, timeout=2):
-      
-        self.comPortConnect= serial.Serial(port=portName, baudrate=baud, timeout=timeout) 
+    def listenerPort (self, portName, baud, bits, bitStop, timeout=2, parity="N"):
+        mgs = "s"
+        try:
+          self.comPortConnect= serial.Serial(port=portName, baudrate=baud, bytesize = int(bits),  parity=parity, stopbits= float (bitStop), timeout=timeout) 
+        except Exception  as e:
+            mgs= "No se pudo abrir el puerto " + portName + ". " , str(e)
+            messagebox.showwarning("Error al abrir el puerto", mgs )
+            return 
+        
+        msg = "Conexion exitosa en el puerto "+portName+ "!"
+        messagebox.showwarning("Genial!", msg)
+
+
+        #Si se conecta, cambiamos todo el ui  
+        #btns and ui
+        self.btnConnect.grid_remove() 
+        self.btnDesconect.grid() 
+        self.footer.grid(column=0, row=30, sticky="w", padx=10, pady=5)
+        self.footer.config(text=f"Escuchando puerto: {portName}")
+        
+        self.sendLabel.grid(column=0, row=2, sticky="w" , padx=15, pady=5)  
+        self.commandInput.grid(column=0, row=3,sticky="w", padx=15 )
+        self.btnSend.grid(column=1, row=3,sticky="w", padx=5 )
+
         while True:  
 
-          #Si se pierde la conexion, cambiamos todo
-          if not self.comPortConnect.is_open or self.comPortConnect == None: 
-             break
-          
-          #Si ahy conexion seguimos leyendo
-          if  self.comPortConnect.inWaiting() > 0:    
-              response =  self.comPortConnect.read_until(b'\r\n').decode('latin-1').strip() 
-              if(response != ""): 
-                self.contentReaderArea.insert(tk.END, f"\n{response}")
-                self.contentReaderArea.see(tk.END)   
-
+            #Si se pierde la conexion, cambiamos todo
+            if not self.comPortConnect.is_open or self.comPortConnect == None: 
+              break
+            
+            #Si ahy conexion seguimos leyendo
+            if self.comPortConnect.is_open  and self.comPortConnect.inWaiting() > 0:    
+                response =  self.comPortConnect.read_until(b'\r\n').decode('latin-1').strip() 
+                if(response != ""): 
+                  self.contentReaderArea.insert(tk.END, f"\n{response}")
+                  self.contentReaderArea.see(tk.END) 
       
     def disconnectComPort(self):  
         #Close port
@@ -41,6 +64,10 @@ class SerialRobotAbner:
         #ChangeVisivily
         self.btnDesconect.grid_remove() 
         self.btnConnect.grid() 
+        #Change send area
+        self.sendLabel.grid( )  
+        self.commandInput.grid(  )
+        self.btnSend.grid(  )
 
     def connectToComPort(self): 
         baud = self.baudInput.get() 
@@ -48,22 +75,29 @@ class SerialRobotAbner:
         stopBit = self.bitStopInput.get() 
         portName = self.ports.get()
 
-        #btns and ui
-        self.btnConnect.grid_remove() 
-        self.btnDesconect.grid() 
-        self.footer.grid(column=0, row=30, sticky="w")
-        self.footer.config(text=f"Escuchando puerto: {portName}")
- 
-        self.threadListener = threading.Thread(target=self.listenerPort, daemon=True, args=[ portName, baud, bits, stopBit])
+        if baud == "" or baud is None :
+           messagebox.showwarning("Error", "Ingrese el baudaje" )
+           return 
+        elif bits == "" or bits is None :
+           messagebox.showwarning("Error", "Ingrese el la cantidad de bits" )
+           return 
+        elif stopBit == "" or stopBit is None :
+           messagebox.showwarning("Error", "Ingrese la cantidad de bits de parada" )
+           return 
+        elif portName == "" or portName is None :
+           messagebox.showwarning("Error", "Seleccione un puerto" )
+           return 
+        
+        self.threadListener = threading.Thread(target=self.listenerPort, daemon=True, args=[ portName, baud, bits , stopBit])
         self.threadListener.start()
 
+        
 
     def sendCommand(self):
         command = self.commandInput.get()
         if command:
            self.comPortConnect.write((command + "\r\n").encode())   
            self.commandInput.delete(0, tk.END)  
-
 
     def keyPressed(self, key):
         actualInputFocus = self.rootUI.focus_get()
@@ -85,20 +119,28 @@ class SerialRobotAbner:
         # Listamos lls puertos
         portComLabel = ttk.Label(rootUI, text="Puerto")
         portComLabel.grid(column=0, row=0,  sticky="nw", padx=15)
-       
         
-        portList  = SerialRobotAbner.getPorts()
-        self.ports = ttk.Combobox(rootUI, values=portList, state="readonly", width=45)   
+        #Spacer 
+        spacer = tk.Frame(self.rootUI, width=350, )  # Ajusta el tamaño del espacio
+        spacer.grid(column=0, row=1, )
+        #Iniciamos sin valores pues llamaos a la funcion setPorts
+        self.ports = ttk.Combobox(rootUI, values=[], state="readonly", width=32)   
         self.ports.grid(column=0, row=1,sticky="nw", padx=15 )
+        self.setPortsToCombo()
         self.ports.current(0)   
+
+        #Refescar button
+        self.btnRefreshComs = ttk.Button(rootUI, width=15, text="Refrescar", command=self.setPortsToCombo)
+        self.btnRefreshComs.grid(column=0, row=1,sticky="e", padx=5 ) 
 
         # Valores baudaje, bits, bit, y paridad
         ##Baudaje
-        baudLAbel = ttk.Label(rootUI, text="Baudaje")
-        baudLAbel.grid(column=1, row=0, sticky="w" , padx=5) 
+        baudLAbel = ttk.Label(rootUI, text="Baudaje",)
+        baudLAbel.grid(column=1, row=0, sticky="w" , padx=5,  ) 
 
-        self.baudInput = ttk.Entry(rootUI, width=15)
+        self.baudInput = ttk.Entry(rootUI, width=15, )
         self.baudInput.grid(column=1, row=1,sticky="w", padx=5 )
+        self.baudInput.insert(0, "9600") 
 
         ##Bits
         bitsLabel = ttk.Label(rootUI, text="Bits")
@@ -106,6 +148,7 @@ class SerialRobotAbner:
 
         self.bitsInput = ttk.Entry(rootUI, width=15)
         self.bitsInput.grid(column=2, row=1,sticky="w", padx=5 )
+        self.bitsInput.insert(0, "8") 
 
         ##Bit To Stop
         stopBitLabel = ttk.Label(rootUI, text="Bits de parada")
@@ -113,6 +156,7 @@ class SerialRobotAbner:
 
         self.bitStopInput = ttk.Entry(rootUI, width=15)
         self.bitStopInput.grid(column=3, row=1,sticky="w", padx=5 )
+        self.bitStopInput.insert(0, "1") 
 
          ##Bit To Stop
         stopBitLabel = ttk.Label(rootUI, text="Bits de parada")
@@ -127,23 +171,16 @@ class SerialRobotAbner:
 
 
         ################  AREA SEND  ###################
-        sendLabel = ttk.Label(rootUI, text="Entrada de datos")
-        sendLabel.grid(column=0, row=2, sticky="w" , padx=15, pady=5) 
-
-        self.commandInput = ttk.Entry(rootUI, width=50)
-        self.commandInput.grid(column=0, row=3,sticky="w", padx=15 )
-
+        self.sendLabel = ttk.Label(rootUI, text="Entrada de datos") 
+        self.commandInput = ttk.Entry(rootUI, width=50)  
         self.btnSend = ttk.Button(rootUI, width=15, text="Enviar", command=self.sendCommand)
-        self.btnSend.grid(column=1, row=3,sticky="w", padx=5 )
-
-
-
-
-        ################  AREA LISTENER  ###################
+      
  
-        self.contentReaderArea = scrolledtext.ScrolledText(rootUI)  
-        self.contentReaderArea.grid(column=0, row=5, columnspan=10, rowspan=6, padx=10, pady=10)
 
+        ################  AREA LISTENER  ################### 
+        self.contentReaderArea = scrolledtext.ScrolledText(rootUI)  
+        self.contentReaderArea.configure(state="normal") 
+        self.contentReaderArea.grid(column=0, row=5, columnspan=10, rowspan=6, padx=10, pady=10) 
   
         ###### FOOOTER ###############  
         self.footer = ttk.Label(rootUI, text=f"Esuchando: { self.comPortConnect}")
